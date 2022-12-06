183676ef89c984860102c96efc45cfbe0301fd64
jr-cast <castanos@pm.me>
2022-12-05 18:58:34 -0600

    Add announcement animation

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,20 +1,18 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  height: 37px;
-  padding: 0.18em;
-  background-color: black;
-  color: white;
-  display: flex;
-  align-items: center;
-  font-size: 0.8em;
-  letter-spacing: 0.1em;
+  margin: 0;
+`;
+
+const Offer = styled.span`
 `;
 
 const Announcement = () => (
-  <Container>
-    FREE SHIPPING within the UK when you spend £50 or more |
-    Express Shipping to the USA from $19.95 | Click here for all FAQs -&gt;
+  <Container className="container">
+    <Offer className="offer">
+      FREE SHIPPING within the UK when you spend £50 or more |
+      Express Shipping to the USA from $19.95 | Click here for all FAQs -&gt;
+    </Offer>
   </Container>
 );
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
-import Slider from '../components/Slider';
-import Products from '../components/Products';
-import Advert from '../components/Advert';
+// import Slider from '../components/Slider';
+// import Products from '../components/Products';
+// import Advert from '../components/Advert';
 
 const Home = () => (
   <div>
     <Announcement />
     <Navbar />
-    <Slider />
+    {/* <Slider />
     <Products />
-    <Advert />
+    <Advert /> */}
   </div>
 );
 
