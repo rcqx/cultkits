d5b5f7bd0e715add9c91f2eb0f2b1ad107bb4e87
jr-cast <castanos@pm.me>
2022-12-09 21:25:35 -0600

    Complete Slide2 Cards

--- src/components/Slider2.jsx
+++ src/components/Slider2.jsx
@@ -1,42 +1,74 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { ArrowRightAlt } from '@material-ui/icons';
 
 const OuterContainer = styled.div`
-  height: 60vh;
+  height: 70vh;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #F5F5F5;
+  padding-left: 1em;
 `;
 
 const Container = styled.div`
   width: 100%;
-  height: 390px;
   display: flex;
-  // overflow: hidden;
+  overflow: hidden;
   display: flex;
   margin: 0.5em;
 `;
 
 const SlidesContainer = styled.div`
   display: flex;
-  padding-left: 0.5em;
 `;
 
 const Slide = styled.div`
   width: 92vw;
+`;
+
+const Card = styled.div`
   margin-right: 0.25em;
 `;
 
-const Slider2 = () => (
-  <OuterContainer>
-    <Container>
-      <SlidesContainer>
-        <Slide id="ad1" />
-        <Slide id="ad2" />
-        <Slide id="ad3" />
-      </SlidesContainer>
-    </Container>
-  </OuterContainer>
-);
+const Description = styled.div`
+  padding: 1.56em;
+  display: flex; 
+  background-color: white;
+  align-items: center;
+`;
 
+const Slider2 = () => {
+  const [touchPosition, setTouchPosition] = useState(null);
+
+  return (
+    <OuterContainer>
+      <Container>
+        <SlidesContainer className="slider-cont">
+          <Card className="slider-card">
+            <Slide id="ad1" />
+            <Description>
+              <p style={{ marginTop: '1em', marginRight: '0.5em' }}>Sign up now and get 10% OFF</p>
+              <ArrowRightAlt style={{ paddingTop: '0.75em' }} />
+            </Description>
+          </Card>
+          <Card className="slider-card">
+            <Slide id="ad2" />
+            <Description>
+              <p style={{ marginTop: '1em', marginRight: '0.5em' }}>Sign up now and get 10% OFF</p>
+              <ArrowRightAlt style={{ paddingTop: '0.75em' }} />
+            </Description>
+          </Card>
+          <Card className="slider-card">
+            <Slide id="ad3" />
+            <Description>
+              <p style={{ marginTop: '1em', marginRight: '0.5em' }}>Sign up now and get 10% OFF</p>
+              <ArrowRightAlt style={{ paddingTop: '0.75em' }} />
+            </Description>
+          </Card>
+        </SlidesContainer>
+      </Container>
+    </OuterContainer >
+  );
+}
 export default Slider2;
