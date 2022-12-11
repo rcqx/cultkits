18d966b7ba8ed5a0ca7b944729e1d469a0506a3c
jr-cast <castanos@pm.me>
2022-12-10 20:49:39 -0600

    Add Mobil and Tablet Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import {
   Search,
   FavoriteBorderOutlined,
   LocalMallOutlined,
+  PersonOutlineOutlined,
 }
   from '@material-ui/icons';
 import { Badge } from '@material-ui/core';
@@ -21,54 +22,87 @@ const Wrapper = styled.div`
   padding: 0.75em 0;
 `;
 
-const Left = styled.div`
+const LeftMobil = styled.div`
   flex: 1;
   display: flex;
   align-item: center;
   justify-content: space-around;
   padding: 0 0.65em;
+
+  @media (min-width: 768px) {
+    display: none; 
+  }
+`;
+
+const LeftTablet = styled.div`
+  flex: 1;
+  display: none;
+  align-item: center;
+  justify-content: start;
+  margin-left: 50px; 
+  @media (min-width: 768px) {
+    display: flex; 
+  }
 `;
 
 const Center = styled.div`
   flex: 2.5;
   text-align: center;
 `;
-const Right = styled.div`
+
+const RightMobil = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: space-around;
   padding: 0 0.65em;
+  @media (min-width: 768px) {
+    display: none; 
+  }
 `;
 
-// const SearchContainer = styled.div`
-//   display: flex;
-//   align-items: center;
-//   margin-left: 1.25em;
-//   padding: 5px;
-//   cursor: pointer;
-// `;
+const RightTablet = styled.div`
+  flex: 1;
+  display: none;
+  align-items: center;
+  justify-content: space-around;
+  padding: 0 0.65em;
+  @media (min-width: 768px) {
+    display: flex; 
+  }
+`;
 
 const Logo = styled.div``;
 
 const Navbar = () => (
   <Container>
     <Wrapper>
-      <Left>
+      <LeftMobil>
         <Menu fontSize="medium" style={{ color: 'black' }} />
         <FavoriteBorderOutlined fontSize="medium" style={{ color: 'black' }} />
-      </Left>
+      </LeftMobil>
+      <LeftTablet class="tablet">
+        <Menu fontSize="large" style={{ color: 'black' }} />
+      </LeftTablet>
       <Center>
         <Logo>
           <img src={logo} alt="logo" style={{ width: '4.5em' }} />
         </Logo>
       </Center>
-      <Right>
+      <RightMobil>
         <Search style={{ color: 'black' }} />
         <Badge badgeContent={4} color="primary">
           <LocalMallOutlined style={{ color: 'black' }} />
         </Badge>
-      </Right>
+      </RightMobil>
+      <RightTablet>
+        <Search style={{ color: 'black' }} />
+        <FavoriteBorderOutlined fontSize="medium" style={{ color: 'black' }} />
+        <PersonOutlineOutlined fontSize="medium" />
+        <Badge badgeContent={4} color="primary">
+          <LocalMallOutlined style={{ color: 'black' }} />
+        </Badge>
+      </RightTablet>
     </Wrapper>
   </Container>
 );
