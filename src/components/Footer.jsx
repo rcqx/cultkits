15523a69af0e77d25b9c817176f60d8bb9739e0c
jr-cast <castanos@pm.me>
2022-12-25 23:48:14 -0700

    Add responsiveness to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,11 +17,20 @@ const List = styled.ul`
   color: white;
   padding: 0;
   margin: 0;
+
+  @media(min-width: 768px) {
+    border: 1px dashed red;
+    width: 30%;
+  }
 `;
 
 const Title = styled.p`
   margin-bottom: 1.5em;
   fon-size: 1.15em;
+
+  @media(min-width: 768px) {
+    font-size: 1.1em;
+  }
 `;
 
 const Item = styled.li`
@@ -29,6 +38,10 @@ const Item = styled.li`
   margin-bottom: 1.25em;
   font-size: 0.95em;
   color: gray;
+
+  @media(min-width: 768px) {
+    width: 300px;
+  }
 `;
 
 const TPContainer = styled.div`
@@ -53,36 +66,51 @@ const SocialIcons = styled.div`
   width: 70%;
   display: flex;
   justify-content: space-between;
+
+  @media(min-width: 762px) {
+    border: 1px solid red; 
+  }
+`;
+
+const ListContainer = styled.div`
+  border: 1px solid red;
+
+  @media(min-width: 768px) {
+    display: flex;
+    align-items: start;
+  }
 `;
 
 const Footer = () => (
   <Container>
-    <List style={{ marginBottom: '3em' }}>
-      <Title>SUPPORT</Title>
-      <Item>FAQ&apos;s</Item>
-      <Item>PRIVACY & COOKIES</Item>
-      <Item>RETURNS</Item>
-    </List>
-    <List style={{ marginBottom: '3em' }}>
-      <Title>USEFUL LINKS</Title>
-      <Item>NEWS & ARTICLES</Item>
-      <Item>SUBSCRIBE TO OUR EMAIL</Item>
-      <Item>ABOUT US</Item>
-      <Item>SELL SHIRTS</Item>
-      <Item>CONTACT US</Item>
-    </List>
-    <List style={{ marginBottom: '3em' }}>
-      <Title>CULT KITS</Title>
-      <Item style={{ fontSize: '1em' }}>Unit 3, The Shaftesbury Centre, Percy Streetm, Swindon, SN2 2AZ, United Kingdom</Item>
-      <Item style={{ fontSize: '1em' }}>
-        VAT No. GB 24045871700
-        <br />
-        Registered Company 10124704.
-      </Item>
-    </List>
-    <TPContainer>
-      <Trustpilot src="https://cdn.shopify.com/s/files/1/0622/3691/7987/files/Trust-pilot-rating-site-promo-4_140x@2x.png?v=1664819133" />
-    </TPContainer>
+    <ListContainer>
+      <List style={{ marginBottom: '3em' }}>
+        <Title>SUPPORT</Title>
+        <Item>FAQ&apos;s</Item>
+        <Item>PRIVACY & COOKIES</Item>
+        <Item>RETURNS</Item>
+      </List>
+      <List style={{ marginBottom: '3em' }}>
+        <Title>USEFUL LINKS</Title>
+        <Item>NEWS & ARTICLES</Item>
+        <Item>SUBSCRIBE TO OUR EMAIL</Item>
+        <Item>ABOUT US</Item>
+        <Item>SELL SHIRTS</Item>
+        <Item>CONTACT US</Item>
+      </List>
+      <List style={{ marginBottom: '3em' }}>
+        <Title>CULT KITS</Title>
+        <Item style={{ fontSize: '1em' }}>Unit 3, The Shaftesbury Centre, Percy Streetm, Swindon, SN2 2AZ, United Kingdom</Item>
+        <Item style={{ fontSize: '1em' }}>
+          VAT No. GB 24045871700
+          <br />
+          Registered Company 10124704.
+        </Item>
+      </List>
+      <TPContainer>
+        <Trustpilot src="https://cdn.shopify.com/s/files/1/0622/3691/7987/files/Trust-pilot-rating-site-promo-4_140x@2x.png?v=1664819133" />
+      </TPContainer>
+    </ListContainer>
     <SocialContainer>
       <SocialIcons>
         <Twitter style={{ color: 'white' }} />
